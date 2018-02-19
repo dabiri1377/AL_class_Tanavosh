89c207212b6098f1cee7b04b99257d7c8151464b
moh <dabiri1377@gmail.com>
2018-02-19 13:02:03 +0330

    edit something

--- session_2/power_func.py
+++ session_2/power_func.py
@@ -1,62 +1,22 @@
-def powerset(input_list, print_set=False):
-    """
-     Get a set as a list and output a list as powerset
-    :param input_list:
-     Input set as a list
-    :param print_set:
-     If want to print final powerset this is True
-    :return:
-    Powerset as output list
-    """
-    output_list = [[]]  # create a list with [] member(all set's have [] member)
+def powerset(set):
 
+    p_set = [{}]
     # scan every list member in input list
-    for list_member in input_list:
+    for x in set:
 
-        temp_list = []  # create a empty temp list
-        # for_debug
-        print("list_member =", list_member)
+        temp_set = {}
+        for sub_set in p_set:
+            temp_set = sub_set | {x}
+            p_set = p_set + [temp_set]
 
-        # scan every member of output list
-        for sub_list in output_list:
-            # Add list_member into every output_list member and add \
-            # it to temp list
-            temp_list += [sub_list + [list_member]]
-            # for_debug
-            print("sub_list =", sub_list)
-            print("temp_list =", temp_list)
+        print(temp_set)
 
-        # add temp list to output list
-        output_list.extend(temp_list)
-        # for_debug
-        print("output_list = ",output_list)
-        print()
-
-    # sort output list with python sort func.
-    output_list.sort()
-
-    # print output powerset if you want
-    if print_set:
-        print(output_list)
-
-    # return output list
-    return output_list
+    return p_set
 
 
 # __main__
 
 
-# get set from user
-input_list_set = input('Enter input set\n').split()
+input_list_set = {1,2,3}
 
-# ask for print powerset
-powerset_print = input('Do you want to print powerset?')
-if powerset_print == 'no' or powerset_print == 'n':
-    powerset_print = 0
-
-# exception handling
-try:
-    powerset(input_list_set, powerset_print)
-except Exception:
-    print("something wrong")
-    print(Exception)
+print(powerset(input_list_set))
