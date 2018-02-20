092f815da00568c592d521050be18ac3067f8366
moh <dabiri1377@gmail.com>
2018-02-20 10:02:38 +0330

    rename some var

--- session_2/power_func.py
+++ session_2/power_func.py
@@ -1,46 +1,46 @@
-def powerset(input_list, print_set=False):
+def powerset(input_set, print_set=False):
     """
      Get a set as a list and output a list as powerset
-    :param input_list:
+    :param input_set:
      Input set as a list
     :param print_set:
      If want to print final powerset this is True
     :return:
     Powerset as output list
     """
-    output_list = [[]]  # create a list with [] member(all set's have [] member)
+    power_set = [[]]  # create a list with [] member(all set's have [] member)
 
     # scan every list member in input list
-    for list_member in input_list:
+    for first_set_members in input_set:
 
         temp_list = []  # create a empty temp list
         # for_debug
-        print("list_member =", list_member)
+        print("first_set_members =", first_set_members)
 
         # scan every member of output list
-        for sub_list in output_list:
-            # Add list_member into every output_list member and add \
+        for sub_list in power_set:
+            # Add first_set_members into every power_set member and add \
             # it to temp list
-            temp_list += [sub_list + [list_member]]
+            temp_list += [sub_list + [first_set_members]]
             # for_debug
             print("sub_list =", sub_list)
             print("temp_list =", temp_list)
 
         # add temp list to output list
-        output_list.extend(temp_list)
+        power_set.extend(temp_list)
         # for_debug
-        print("output_list = ",output_list)
+        print("power_set = ",power_set)
         print()
 
     # sort output list with python sort func.
-    output_list.sort()
+    power_set.sort()
 
     # print output powerset if you want
     if print_set:
-        print(output_list)
+        print(power_set)
 
     # return output list
-    return output_list
+    return power_set
 
 
 # __main__
